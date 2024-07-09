6512030c1d5651edb7ee0075a61c783f74015a33
Matheus <mreinheimer0@gmail.com>
2024-07-08 23:32:54 -0300

    feat: create styles of first page

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,55 @@
-function App() {
+import { ArrowRight, Calendar, MapPin } from 'lucide-react'
+
+export function App() {
   return (
-    <>
-      <h1 className="text-3xl text-teal-300">Hello World</h1>
-    </>
+    <div className="bg-pattern flex h-screen w-full items-center justify-center bg-center bg-no-repeat">
+      <div className="w-full max-w-3xl space-y-10 px-6 text-center">
+        <div className="flex flex-col items-center gap-3">
+          <img src="/logo.svg" alt="plann.er" />
+          <p className="zinc-300 text-lg">
+            Convide seus amigos e planeje sua próxima viagem!
+          </p>
+        </div>
+
+        <div className="shadow-shape flex h-16 items-center gap-3 rounded-xl bg-zinc-900 px-4">
+          <div className="flex flex-1 items-center gap-2">
+            <MapPin className="size-5 text-zinc-400" />
+            <input
+              type="text"
+              placeholder="Para onde você vai?"
+              className="placeholder:zinc-400 flex-1 bg-transparent text-lg outline-none"
+            />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Calendar className="size-5 text-zinc-400" />
+            <input
+              type="text"
+              placeholder="Quando?"
+              className="placeholder:zinc-400 w-40 bg-transparent text-lg outline-none"
+            />
+          </div>
+
+          <div className="h-6 w-px bg-zinc-800" />
+
+          <button className="flex items-center gap-2 rounded-lg bg-lime-300 px-5 py-2 font-medium text-lime-950 hover:bg-lime-400">
+            Continuar <ArrowRight className="size-5" />
+          </button>
+        </div>
+
+        <p className="text-sm text-zinc-500">
+          Ao planejar sua viagem pela plann.er você automaticamente concorda{' '}
+          <br /> com nossos{' '}
+          <a href="#" className="text-zinc-300 underline">
+            termos de uso
+          </a>{' '}
+          e{' '}
+          <a href="#" className="text-zinc-300 underline">
+            políticas de privacidade
+          </a>
+          .
+        </p>
+      </div>
+    </div>
   )
 }
-
-export default App

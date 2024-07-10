ac6608c4e6c29bacdcb043978d8e7749aa0bf180
Matheus <mreinheimer0@gmail.com>
2024-07-09 22:45:26 -0300

    feat: Refactor trip details page to use ImportantLinks component

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,16 +1,8 @@
-import {
-  Calendar,
-  CircleCheck,
-  CircleDashed,
-  Link2,
-  MapPin,
-  Plus,
-  Settings2,
-  UserCog,
-} from 'lucide-react'
+import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from 'lucide-react'
 import { useState } from 'react'
 
 import { CreateActivityModal } from './create-activity-modal'
+import { ImportantLinks } from './important-links'
 
 export function TripDetailsPage() {
   const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
@@ -91,77 +83,7 @@ export function TripDetailsPage() {
             </div>
           </div>
         </div>
-        <div className="w-80 space-y-6">
-          <div className="space-y-6">
-            <h2 className="text-xl font-semibold">Links importantes</h2>
-            <div className="space-y-5">
-              <div className="flex items-center justify-between gap-4">
-                <div className="space-y-1.5">
-                  <span className="block font-medium text-zinc-100">
-                    Reserva do AirBnB
-                  </span>
-                  <a
-                    href="#"
-                    className="block truncate text-xs text-zinc-400 hover:text-zinc-200"
-                  >
-                    https://www.airbnb.com.br/rooms/123456
-                  </a>
-                </div>
-                <Link2 className="size-5 shrink-0 text-zinc-400" />
-              </div>
-              <div className="flex items-center justify-between gap-4">
-                <div className="space-y-1.5">
-                  <span className="block font-medium text-zinc-100">
-                    Reserva do AirBnB
-                  </span>
-                  <a
-                    href="#"
-                    className="block truncate text-xs text-zinc-400 hover:text-zinc-200"
-                  >
-                    https://www.airbnb.com.br/rooms/123456
-                  </a>
-                </div>
-                <Link2 className="size-5 shrink-0 text-zinc-400" />
-              </div>
-            </div>
-            <button className="judstify-center flex h-11 w-full items-center gap-2 rounded-lg bg-zinc-800 px-5 font-medium text-zinc-200 hover:bg-zinc-700">
-              <Plus className="size-5" />
-              Cadastrar novo link
-            </button>
-          </div>
-          <div className="h-px w-full bg-zinc-800" />
-          <div className="space-y-6">
-            <h2 className="text-xl font-semibold">Convidados</h2>
-            <div className="space-y-5">
-              <div className="flex items-center justify-between gap-4">
-                <div className="space-y-1.5">
-                  <span className="block font-medium text-zinc-100">
-                    Jessica White
-                  </span>
-                  <span className="block truncate text-sm text-zinc-400">
-                    jessica.white@gmail.com
-                  </span>
-                </div>
-                <CircleDashed className="size-5 shrink-0 text-zinc-400" />
-              </div>
-              <div className="flex items-center justify-between gap-4">
-                <div className="space-y-1.5">
-                  <span className="block font-medium text-zinc-100">
-                    Dr. Rita Pacocha
-                  </span>
-                  <span className="block truncate text-sm text-zinc-400">
-                    Lacy.stiedemann@gmail.com
-                  </span>
-                </div>
-                <UserCog className="size-5 shrink-0 text-zinc-400" />
-              </div>
-            </div>
-            <button className="flex h-11 w-full items-center justify-center gap-2 rounded-lg bg-zinc-800 px-5 font-medium text-zinc-200 hover:bg-zinc-700">
-              <Plus className="size-5" />
-              Gerenciar convidados
-            </button>
-          </div>
-        </div>
+        <ImportantLinks />
       </main>
 
       {isCreateActivityModalOpen && (
